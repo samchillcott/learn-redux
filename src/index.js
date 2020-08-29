c07a6add2f45c247e182355415dd4c2524a0b5fc
Sam Chillcott <sam@samchillcott.com>
2020-08-29 12:08:18 +0100

    Provider

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { createStore } from "redux";
 import allReducers from "./reducers";
+import { Provider } from 'react-redux';
 
 const store = createStore(
 	allReducers,
@@ -12,9 +13,9 @@ const store = createStore(
 );
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+		<Provider store={store}>
+      <App />
+    </Provider>,
 	document.getElementById("root")
 );
 
