0599f712d60c2d8b9a3904cb534615c727846a6d
Sam Chillcott <sam@samchillcott.com>
2020-08-29 12:33:57 +0100

    useSelector

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import {useSelector} from 'react-redux';
 
 function App() {
+  
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
   return (
     <div className="App">
-        <h1>Hello World</h1>
+        <h1>Counter {counter}</h1>
+
+        {isLogged ? <h3>Valuable info you shouldn't see if not logged in</h3> : ''}
     </div>
   );
 }
