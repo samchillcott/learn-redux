10d49d9fd80e4a9c2b6f1f7585051e7b2f733226
Sam Chillcott <sam@samchillcott.com>
2020-08-29 12:46:51 +0100

    useDispatch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment } from "./actions";
 
 function App() {
-  
-  const counter = useSelector(state => state.counter);
-  const isLogged = useSelector(state => state.isLogged);
-  return (
-    <div className="App">
-        <h1>Counter {counter}</h1>
-
-        {isLogged ? <h3>Valuable info you shouldn't see if not logged in</h3> : ''}
-    </div>
-  );
+	const counter = useSelector((state) => state.counter);
+	const isLogged = useSelector((state) => state.isLogged);
+	const dispatch = useDispatch();
+	return (
+		<div className="App">
+			<h1>Counter {counter}</h1>
+			<button onClick={() => dispatch(increment())}>+</button>
+			<button>-</button>
+			{isLogged ? (
+				<h3>Valuable info you shouldn't see if not logged in</h3>
+			) : (
+				""
+			)}
+		</div>
+	);
 }
 
 export default App;
