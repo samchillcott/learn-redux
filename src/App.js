0c4d52caaa0efea2675161ca90c29d40286e2097
Sam Chillcott <sam@samchillcott.com>
2020-08-29 13:09:28 +0100

    increment payload

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 	return (
 		<div className="App">
 			<h1>Counter {counter}</h1>
-			<button onClick={() => dispatch(increment())}>+</button>
+			<button onClick={() => dispatch(increment(5))}>+</button>
 			<button onClick={() => dispatch(decrement())}>-</button>
 			{isLogged ? (
 				<h3>Valuable info you shouldn't see if not logged in</h3>
